9918be4a372d462978e434458c1517a238c745da
Gerald G <geraldguignard2@gmail.com>
2026-05-19 10:22:37 -0400

    Made the 10 list of peopel then made a method to look for name the user puts in.

--- src/main/java/com/pluralsight/loops/Person.java
+++ src/main/java/com/pluralsight/loops/Person.java
@@ -1,4 +1,38 @@
 package com.pluralsight.loops;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private double age;
+
+
+    public Person(String firstName, String lastName, double age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -1,7 +1,56 @@
 package com.pluralsight.loops;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+
+        Person person1 = new Person("James", "Carter", 34);
+        people.add(person1);
+        Person person2 = new Person("Sofia", "Nguyen", 27);
+        people.add(person2);
+        Person person3 = new Person("Marcus", "Thompson", 41);
+        people.add(person3);
+        Person person4 = new Person("Elena", "Rodriguez", 23);
+        people.add(person4);
+        Person person5 = new Person("David", "Kim", 55);
+        people.add(person5);
+        Person person6 = new Person("Priya", "Patel", 31);
+        people.add(person6);
+        Person person7 = new Person("Tyler", "Brooks", 19);
+        people.add(person7);
+        Person person8 = new Person("Amara", "Johnson", 38);
+        people.add(person8);
+        Person person9 = new Person("Lucas", "Fernandez", 46);
+        people.add(person9);
+        Person person10 = new Person("Hannah", "Lee", 22);
+        people.add(person10);
+
+        Scanner scanner = new Scanner(System.in);
+
+        lookingForPeople(scanner, people);
+
 
     }
+
+    public static void lookingForPeople(Scanner scanner, List<Person> people) {
+        System.out.println("What name are you looking for: ");
+        String name = scanner.nextLine();
+
+        List<Person> match = new ArrayList<>();
+
+        for (Person person : people){
+            if (person.getFirstName().equalsIgnoreCase(name) || (person.getLastName().equalsIgnoreCase(name))) {
+                match.add(person);
+            }
+        }
+        for (Person person : match) {
+            System.out.println("Is this the person you are looking for?: " + person);
+        }
+    }
+
+
 }
